60ddcb731fb03811b505998e46f26aebb122cdcb
IT22362476 <denuwansathsara0412@gmail.com>
2026-03-22 15:35:24 +0530

    image added

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -58,7 +58,7 @@ const paymentController = {
               name: p.menuItemName,
               images: p.image
                 ? [p.image]
-                : ["https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"],
+                : ["https://images.unsplash.com/photo-1549451371-64aa98a6f660?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"],
             },
             unit_amount: Math.round(p.price * 100),
           },
